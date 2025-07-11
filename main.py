400d0402dff03697251c7dddf6dd2489c03d4e4f
7700K <751510724@qq.com>
2025-07-12 06:52:49 +0800

    Your commit message

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-print("Hello, World!")
+import torch
+print(torch.__version__)  # 打印 PyTorch 版本
+print(torch.cuda.is_available())  # 如果返回 True，表示 CUDA 可用
+print(torch.cuda.get_device_name(0))  # 显示 GPU 名称
